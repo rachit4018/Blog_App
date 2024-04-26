a6fa93d51d33e7a65a11eafaa7c69ffe02389e0d
rachit4018 <rachitpandya2509@gmail.com>
2024-04-26 11:01:32 -0400

    update blog feature added

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import BlogPostView,BlogDetailView
+from .views import BlogPostView,BlogDetailView,BlogCreateView, BlogUpdateView
 urlpatterns = [
     path('',BlogPostView.as_view(),name='home'),
     path('post/<int:pk>/',BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/',BlogCreateView.as_view(), name = 'post_new'),
+    path('post/update/<int:pk>/', BlogUpdateView.as_view(), name='post_update'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from django.views.generic import ListView,DetailView
+from django.views.generic.edit import CreateView,UpdateView
 from .models import Post
 # Create your views here.
 
@@ -11,3 +12,13 @@ class BlogPostView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'text']
+
+class BlogUpdateView(UpdateView):
+    model =Post
+    template_name = 'post_update.html'
+    fields = ['title','text']
